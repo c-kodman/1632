f6ae885677167da630618a0ae842bcc8a5b3fc7e
Craig <cmk126@pitt.edu>
2016-03-24 14:43:13 -0400

    joel is god

---
+++ deliverable4/test/test.java
@@ -0,0 +1,64 @@
+//cmk126 Craig Kodman
+        import static org.junit.Assert.*;
+
+        import java.util.Arrays;
+        import java.util.Random;
+
+        import org.junit.Before;
+        import org.junit.Test;
+//import org.junit.Test;
+public class test {
+    private int[] arr;
+
+    //public static void main(String[] args) {
+    @Before
+    public void setup() {
+        Random rand = new Random();
+        //ArrayList<int[]> al = new ArrayList<int[]>();
+        int i;
+        int j;
+        int r;
+        for (i = 0; i < 106; i++) {
+            r = rand.nextInt(5) + 1;
+            arr = new int[r];
+            for (j = 0; j < arr.length; j++) {//generate random
+                arr[j] = rand.nextInt(200) - 100;
+
+            }
+            //  System.out.println(Arrays.toString(arr));
+//            testPure(arr);
+            //  Arrays.sort(arr);
+            //System.out.println(Arrays.toString(arr));
+        }
+    }
+    //}
+    @Test
+    public void testPure()
+    {
+        int [] arrayOne = arr;
+        int []arrayTwo =  Arrays.copyOf(arr, arr.length);
+        Arrays.sort(arrayOne);
+        Arrays.sort(arrayTwo);
+        assertTrue(Arrays.equals(arrayOne, arrayTwo));
+
+    }
+    @Test
+    public void testLength(){
+        int unsortedArray = arr.length;
+        System.out.println(Arrays.toString(arr));
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        int sortedArray = arr.length;
+        assertEquals(unsortedArray, sortedArray);
+    }
+
+    @Test
+    public void idempotent(){
+        int [] arrayOne = arr;
+        Arrays.sort(arrayOne);
+        int []arrayTwo =  Arrays.copyOf(arrayOne, arrayOne.length);
+        Arrays.sort(arrayTwo);
+        assertArrayEquals(arrayOne, arrayTwo);
+    }
+
+}
